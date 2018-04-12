07f92afb6ef4036d7226700b1baaf166c9bd19cd
yanglikai <yanglikai@yonghui.cn>
2018-04-12 14:42:03 +0800

    #yanglikai #2018-04-12 #add model muyclound-consumer

---
+++ muyclound-consumer/muyclound-consumer-b2b/src/main/java/com/muyclound/consumer/MuycloundConsumerB2bApplication.java
@@ -0,0 +1,12 @@
+package com.muyclound.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuycloundConsumerB2bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuycloundConsumerB2bApplication.class, args);
+	}
+}
---
+++ muyclound-consumer/muyclound-consumer-s2b/src/main/java/com/muyclound/consumer/MuycloundConsumerS2bApplication.java
@@ -0,0 +1,12 @@
+package com.muyclound.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuycloundConsumerS2bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuycloundConsumerS2bApplication.class, args);
+	}
+}
