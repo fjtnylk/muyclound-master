4379b0820c1a63b9ae8b7c72de5441162e64f189
yanglikai <yanglikai@yonghui.cn>
2018-04-12 14:30:42 +0800

    #yanglikai #2018-04-12 #eureka集群搭建

--- muyclound-provider/muyclound-provider-umc/src/main/java/com/muyclound/MuycloundProviderUmcApplication.java
+++ muyclound-provider/muyclound-provider-umc/src/main/java/com/muyclound/MuycloundProviderUmcApplication.java
@@ -2,11 +2,26 @@ package com.muyclound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MuycloundProviderUmcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MuycloundProviderUmcApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MuycloundProviderUmcApplication.class, args);
+  }
+
+  @RestController
+  public static class SimpleStoresController {
+    @RequestMapping("/simple/stores")
+    @ResponseBody
+    String getStores() {
+      return "yanglikai";
+    }
+  }
 }
