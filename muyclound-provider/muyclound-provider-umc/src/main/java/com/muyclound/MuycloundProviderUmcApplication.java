03f5e68390e838cd5cba3740aa9345c5419e9f76
yanglikai <yanglikai@yonghui.cn>
2018-04-13 15:47:16 +0800

    #yanglikai #2018-04-12 #eureka集群搭建

--- muyclound-provider/muyclound-provider-umc/src/main/java/com/muyclound/MuycloundProviderUmcApplication.java
+++ muyclound-provider/muyclound-provider-umc/src/main/java/com/muyclound/MuycloundProviderUmcApplication.java
@@ -3,10 +3,13 @@ package com.muyclound;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,9 +19,15 @@ public class MuycloundProviderUmcApplication {
     SpringApplication.run(MuycloundProviderUmcApplication.class, args);
   }
 
+  @LoadBalanced
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
   @RestController
   public static class SimpleStoresController {
-    @RequestMapping("/simple/stores")
+    @RequestMapping("/yanglikai")
     @ResponseBody
     String getStores() {
       return "yanglikai";
